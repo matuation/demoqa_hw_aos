4b74dd140a574db7ca6745a295e44c833ec6ab94
Aleksey Schastlivtsev <matuation@gmail.com>
2025-11-29 17:59:51 +0300

    updated build.gradle with JUnit5 created PracticeFormTests.java class with blank code

---
+++ src/test/java/PracticeFormTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void BeforeAll() {
+
+    }
+
+    @Test
+    void successfulFormFillTest(){
+
+    }
+}
