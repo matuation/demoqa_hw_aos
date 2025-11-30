279c461c0c41b39e57d4095d94459dbddb688f8c
Aleksey Schastlivtsev <matuation@gmail.com>
2025-11-30 21:50:48 +0300

    commented holdBrowserOpen, timeout

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -19,8 +19,8 @@ public class PracticeFormTests {
         //Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        //Configuration.holdBrowserOpen = true;
+        //Configuration.timeout = 5000; // default 4000
         new ScrollTo();
     }
 
