ed99aa01422b738df5414339dba44a45ca1f2acb
Aleksey Schastlivtsev <matuation@gmail.com>
2026-01-15 19:16:40 +0300

    1. switched project type to simple tests from lesson 2. updated build.gradle to use allure

---
+++ src/test/java/SkippedTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void someTest() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void someTest1() {
+        assertTrue(false);
+    }
+
+}
